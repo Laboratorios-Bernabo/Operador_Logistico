67e090ff63dea5442c207960d730c6a5d8425805
Santiago Balbiani <santiagobalbiani@hotmail.com>
2021-03-26 08:41:36 -0300

    Documentation ok

---
+++ operador_logistico/controllers/documentation.js
@@ -0,0 +1,9 @@
+
+const controller = {
+    getDocumentation: (req, res) => {
+        return res.render('documentation');
+    }
+}
+
+
+module.exports = controller;
--- operador_logistico/routes/index.js
+++ operador_logistico/routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
-
+const documentationController = require('../controllers/documentation');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+router.get('/documentation', documentationController.getDocumentation);
 
 module.exports = router;
