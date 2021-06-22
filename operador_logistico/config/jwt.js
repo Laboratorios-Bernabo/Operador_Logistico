21ff6b25187ba33d1dd256ee99953522fcee99d3
Santiago Balbiani <santiagobalbiani@hotmail.com>
2021-06-22 01:29:08 -0300

    leave out 124 int unprotected

--- operador_logistico/config/jwt.js
+++ operador_logistico/config/jwt.js
@@ -3,15 +3,16 @@ const config = require('../config.json');
 
 module.exports = jwt;
 
+const unprotected = [
+        pathToRegexp('/out/124*'),
+        '/auth/login',
+        '/auth/access',
+        '/documentation',
+        '/out/getStock'];
+
 function jwt() {
     const { secret } = config;
     return expressJwt({ secret, algorithms: ['HS256'] }).unless({
-        path: [
-            // public routes that don't require authentication
-            '/auth/login',
-            '/auth/access',
-            '/documentation',
-            '/out/getStock'
-        ]
+        path: unprotected
     });
 }
